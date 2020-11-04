e704e6c2ce071242676c8ba01cce3f58f1235098
Vasil Dimitrov <v.b.dimitrow@gmail.com>
2020-11-04 11:54:38 +0200

    Added problems

---
+++ InterfacesAndAbstractionExcercise/DefineanInterfaceIPerson/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using PersonInfo;
+using System;
+
+namespace PersonInfo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            string id = Console.ReadLine();
+            string birthdate = Console.ReadLine();
+
+            Citizen citizen = new Citizen(name, age, id, birthdate);
+
+            //Console.WriteLine(citizen.Name);
+           // Console.WriteLine(citizen.Age);
+            Console.WriteLine(citizen.Id);
+            Console.WriteLine(citizen.Birthdate);
+        }
+    }
+}
