7dcb6ddb4f7714423b7af58f84622c6e2e740341
Vasil Dimitrov <v.b.dimitrow@gmail.com>
2020-11-04 10:52:16 +0200

    Delete unused files

---
+++ InterfacesAndAbstraction/Shapes/Rectangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        private int width;
+        private int height;
+
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public void Draw()
+        {
+            
+        }
+    }
+}
