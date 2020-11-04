e6f0aac6bed5cc7ee5cfe8889b9377edf2fec1b9
Vasil Dimitrov <v.b.dimitrow@gmail.com>
2020-11-04 11:18:59 +0200

    Added Cars problem

---
+++ InterfacesAndAbstraction/Cars/Contracts/ICar.cs
@@ -0,0 +1,11 @@
+﻿namespace Cars
+{
+    public interface ICar
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public void Start();
+        public void Stop();
+    }
+}
---
+++ InterfacesAndAbstraction/Cars/Models/Seat.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars.Models
+{
+    public class Seat : ICar
+    {
+        public Seat(string model, string color)
+        {
+            Model = model;
+            Color = color;
+        }
+
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public void Start()
+        {
+            Console.WriteLine("Engine start");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Breaaak!");
+        }
+
+        public override string ToString()
+        {
+            return $"{Color} {this.GetType().Name} {Model}";
+        }
+    }
+}
