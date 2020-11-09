fa9db4a1f349675dbbd9180f4bfbf2b6f401e323
Vasil Dimitrov <v.b.dimitrow@gmail.com>
2020-11-09 08:16:10 +0200

    Problem added

---
+++ InterfacesAndAbstractionExcercise/ExplicitInterfaces/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using ExplicitInterfaces.Interfaces;
+using ExplicitInterfaces.Models;
+using System;
+
+namespace ExplicitInterfaces
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string input;
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] personInfo = input.Split();
+
+                string name = personInfo[0];
+                string country = personInfo[1];
+                int age = int.Parse(personInfo[2]);
+
+                Citizen citizen = new Citizen(name, country, age);
+                IPerson person = citizen;
+                IResident resident = citizen;
+
+                Console.WriteLine(person.GetName());
+                Console.WriteLine(resident.GetName());
+            }
+        }
+    }
+}
