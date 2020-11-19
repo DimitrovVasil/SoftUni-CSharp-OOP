bd3961d4a91f37fa141f1ad079f63f0b5b0abbe0
Vasil Dimitrov <v.b.dimitrow@gmail.com>
2020-11-19 10:49:30 +0200

    problem extended

--- ReflectionAndAttributes/AuthorProblem/StartUp.cs
+++ ReflectionAndAttributes/AuthorProblem/StartUp.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Reflection;
 
 namespace AuthorProblem
 {
     public class StartUp
     {
+        [Author("Vasil Bay")]
         public static void Main(string[] args)
         {
-            
+            Tracker tracker = new Tracker();
+            Tracker.PrintMethodsByAuthor();
+
         }
     }
 }
---
+++ ReflectionAndAttributes/AuthorProblem/Tracker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Reflection;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        
+        public static void PrintMethodsByAuthor()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            Type type = typeof(StartUp);
+            var attributes = assembly.GetCustomAttributes();
+
+            var methods = type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+
+            foreach (var method in methods)
+            {
+                if (method.CustomAttributes.Any(a => a.AttributeType == typeof(AuthorAttribute)))
+                {
+                    var methodAttributes = method.GetCustomAttributes();
+                    Attribute targetAttr = methodAttributes.FirstOrDefault(a => a.GetType() == typeof(AuthorAttribute));
+                    var finalAttr = targetAttr as AuthorAttribute;
+                    Console.WriteLine($"{method.Name} is written by {finalAttr.Name}");
+                }
+
+            }
+
+        }
+    }
+}
