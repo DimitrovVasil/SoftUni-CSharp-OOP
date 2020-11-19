57750223c68c01290a52d5b448338763b6210f50
Vasil Dimitrov <v.b.dimitrow@gmail.com>
2020-11-19 15:44:59 +0200

    Problem added

--- UnitTestingExcercise/FightingArena.Tests/ArenaTests.cs
+++ UnitTestingExcercise/FightingArena.Tests/ArenaTests.cs
@@ -1,4 +1,6 @@
 ﻿using NUnit.Framework;
+using FightingArena;
+using System;
 
 namespace Tests
 {
@@ -10,9 +12,76 @@ namespace Tests
         }
 
         [Test]
-        public void Test1()
+        public void WarriorListShouldBeNotNullWhenArenaIsIntancing()
         {
-            Assert.Pass();
+            Arena arena = new Arena();
+
+            Assert.That(arena.Warriors != null);
+        }
+
+        [Test]
+        public void ArenaShouldThrowExceptionWhenTryToAddWarriorAlreadyExisting()
+        {
+            Arena arena = new Arena();
+            Warrior warrior1 = new Warrior("Djukata", 40, 50);
+            Warrior warrior2 = new Warrior("Djukata", 60, 70);
+            arena.Enroll(warrior1);
+            
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                arena.Enroll(warrior2);
+            });
+        }
+
+        [Test]
+        public void ArenaShouldAddWarriorSuccessfully()
+        {
+            Arena arena = new Arena();
+            Warrior warrior1 = new Warrior("Djukata", 40, 50);
+            Warrior warrior2 = new Warrior("Bitalsa", 60, 70);
+            arena.Enroll(warrior1);
+            arena.Enroll(warrior2);
+
+            Assert.That(arena.Warriors.Count == 2);
+        }
+
+        [TestCase(null, "Tosho")]
+        [TestCase("Tosho", null)]
+        [TestCase("Tosho", "Denko")]
+        [TestCase("Denko", "Tosho")]
+
+        public void ShouldThrowExceptionWhenWarriorIsMissing(string firstWarrior, string secondWarrior)
+        {
+            Warrior warrior1 = new Warrior("Tosho", 40, 50);
+            Warrior warrior2 = new Warrior("Gosho", 60, 70);
+
+            Arena arena = new Arena();
+
+            arena.Enroll(warrior1);
+            arena.Enroll(warrior2);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                arena.Fight(firstWarrior, secondWarrior);
+            });
+
+        }
+
+      
+        [TestCase("Valio", "Gosho")]
+        public void ShouldReturnFirstWarriorAsMissing(string firstWarrior, string secondWarrior)
+        {
+            Warrior warrior1 = new Warrior("Tosho", 40, 50);
+            Warrior warrior2 = new Warrior("Gosho", 60, 70);
+
+            Arena arena = new Arena();
+
+            arena.Enroll(warrior1);
+            arena.Enroll(warrior2);
+
+            var expectedException = $"There is no fighter with name {firstWarrior} enrolled for the fights!";
+            var actualException = Assert.Throws<InvalidOperationException>(() => arena.Fight(firstWarrior, secondWarrior));
+            Assert.AreEqual (expectedException, actualException.Message);
         }
     }
 }
--- UnitTestingExcercise/FightingArena.Tests/WarriorTests.cs
+++ UnitTestingExcercise/FightingArena.Tests/WarriorTests.cs
@@ -1,7 +1,10 @@
 using NUnit.Framework;
+using System;
+using FightingArena;
 
 namespace Tests
 {
+    [TestFixture]
     public class WarriorTests
     {
         [SetUp]
@@ -9,10 +12,125 @@ namespace Tests
         {
         }
 
-        [Test]
-        public void Test1()
+        [TestCase("Tisho", 10, 50)]
+        public void ConstructorShouldWorksCorrectly(string name, int damage, int hp)
         {
-            Assert.Pass();
+            Warrior warrior = new Warrior(name, damage, hp);
+
+            Assert.AreEqual(name, warrior.Name);
+            Assert.AreEqual(damage, warrior.Damage);
+            Assert.AreEqual(hp, warrior.HP);      
+        }
+
+        [TestCase("   ", 10, 50)]
+        [TestCase("", 10, 50)]
+        [TestCase(null, 10, 50)]
+        public void NameShouldThrowsArgumentException(string name, int damage, int hp)
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                Warrior warrior = new Warrior(name, damage, hp);
+            });
+        }
+
+        [TestCase("Pesho", -1, 50)]
+        [TestCase("Pesho", 0, 50)]
+        [TestCase("Pesho", -10, 50)]
+        public void DamageShouldThrowsArgumentException(string name, int damage, int hp)
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                Warrior warrior = new Warrior(name, damage, hp);
+            });
+        }
+
+        [TestCase("Pesho", 10, -1)]
+        public void HPShouldThrowsArgumentException(string name, int damage, int hp)
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                Warrior warrior = new Warrior(name, damage, hp);
+            });
+        }
+
+        [TestCase("Pesho", 40, 10)]
+        [TestCase("Pesho", 40, 29)]
+        [TestCase("Pesho", 40, 30)]
+
+        public void WarriorShouldThrowExceptionWhenHisHPAreBelowOrEqual30AndTryAttack(string name, int damage, int hp)
+        {
+            Warrior warrior1 = new Warrior(name, damage, hp);
+            Warrior warrior2 = new Warrior("Tosho", 50, 40);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                warrior1.Attack(warrior2);
+            });
+        }
+
+        [TestCase("Pesho", 40, 10)]
+        [TestCase("Pesho", 40, 29)]
+        [TestCase("Pesho", 40, 30)]
+
+        public void WarriorShouldThrowExceptionWhenVictimHPAreBelowOrEqual30AndTryAttack(string name, int damage, int hp)
+        {
+            Warrior warrior1 = new Warrior("Tosho", 50, 40);
+            Warrior warrior2 = new Warrior(name, damage, hp);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                warrior1.Attack(warrior2);
+            });
+        }
+
+        [TestCase("Pesho", 60, 10)]
+        public void WarriorShouldThrowExceptionWhenVictimDamageAreBiggerThanHisHP(string name, int damage, int hp)
+        {
+            Warrior warrior1 = new Warrior("Tosho", 50, 40);
+            Warrior warrior2 = new Warrior(name, damage, hp);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                warrior1.Attack(warrior2);
+            });
+        }
+
+        [Test]
+        public void WarriorShouldLoseHP()
+        {
+            Warrior attacker = new Warrior("Tosho", 60, 80);
+            Warrior victim = new Warrior("Pesho", 45, 40);
+
+            attacker.Attack(victim);
+
+            Assert.That(attacker.HP == 35);
+        }
+
+        [Test]
+        public void EnemyHPShouldBecome0WhenWarriorDamageIsBigger()
+        {
+            Warrior attacker = new Warrior("Tosho", 80, 80);
+            Warrior enemy = new Warrior("Pesho", 60, 50);
+            attacker.Attack(enemy);
+            Assert.That(enemy.HP == 0);
+        }
+
+        [Test]
+        public void EnemyHPShouldDecreaseWithWarriorDamageWhenIsBigger()
+        {
+            Warrior attacker = new Warrior("Tosho", 40, 80);
+            Warrior enemy = new Warrior("Pesho", 60, 50);
+            attacker.Attack(enemy);
+            Assert.That(enemy.HP == 10);
+        }
+
+        [Test]
+        public void EnemyHPShouldDecreaseWithWarriorDamageWhenIsEqual()
+        {
+            Warrior attacker = new Warrior("Tosho", 40, 80);
+            Warrior enemy = new Warrior("Pesho", 60, 40);
+            attacker.Attack(enemy);
+            Assert.That(enemy.HP == 0);
         }
     }
 }
